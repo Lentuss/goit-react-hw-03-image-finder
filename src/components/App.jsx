fff1c24031e56fa614f0323d9460762ccccb6b79
Olena Pohybil <itlentus@gmail.com>
2022-07-15 21:04:55 +0300

    add files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { GlobalStyle } from './GlobalStyled';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <GlobalStyle />
+        <div>Image finder</div>
+      </>
+    );
+  }
+}
+
+export default App;
